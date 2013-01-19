894933ddd6708802f760f03e398e12f2f037115f
Rob Schoening <robschoening@gmail.com>
2013-01-18 16:38:06 -0800

    fixed failing test

--- src/it/bintray-it/verify.groovy
+++ src/it/bintray-it/verify.groovy
@@ -1,7 +1,7 @@
 
 // This file will check that test.txt was created by gradle
 
-File f = new File(basedir,"output.txt");
+File f = new File(basedir,"/build/output.txt");
 
 if (!f.exists()) {
 	throw new FileNotFoundException("File not found: "+f.getAbsolutePath())
