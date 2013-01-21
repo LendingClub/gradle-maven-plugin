1829e3716da4ec398c23b1c490d5d802d338338d
EC2 Default User <ec2-user@domU-12-31-39-0B-D9-30.compute-1.internal>
2013-01-21 18:05:45 +0000

    removed bintray-it.  will replace with examples

--- src/it/bintray-it/verify.groovy
+++
@@ -1,8 +0,0 @@
-
-// This file will check that test.txt was created by gradle
-
-File f = new File(basedir,"/build/output.txt");
-
-if (!f.exists()) {
-	throw new FileNotFoundException("File not found: "+f.getAbsolutePath())
-}
